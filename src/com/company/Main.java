8c5a162b6cf1f1a3baa51f6ac7250536dee92a23
olegt <oleg.tokarev.89@inbox.ru>
2021-02-12 11:41:42 +0300

    Hello World2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.util.function.IntUnaryOperator;
+
 public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello world1");
-        System.out.println("Hello world2");
 
+        meow Meow = () -> 1.0;
     }
 }
