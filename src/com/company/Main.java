b162a778c74b21108dc3404c42ab22b7dc33ff41
olegt <oleg.tokarev.89@inbox.ru>
2021-02-12 21:57:01 +0300

    this code shows how we can use lambda expressions in java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,32 @@
 package com.company;
 
-import java.util.function.IntUnaryOperator;
-
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world1");
 
-        meow Meow = () -> 1.0;//
+        String someStr = "Java is good language-+-";
+
+        StringFunc reverse = (str)->{
+            String result = "";
+            for(int i = str.length()-1; i>=0; i--){
+                result+=str.charAt(i);
+            }
+            return result;
+        };
+        StringFunc changeCase = (str)->{
+            String result = "";
+            char ch;
+            for(int i = 0; i<=str.length()-1; i++){
+                ch = str.charAt(i);
+                if(Character.isUpperCase(ch))
+                    result+=Character.toLowerCase(ch);
+                else
+                    result+=Character.toUpperCase(ch);
+            }
+            return result;
+        };
+        System.out.println(ChangeMethod.changeString(reverse,someStr));
+        System.out.println(ChangeMethod.changeString(changeCase,someStr));
+        System.out.println(ChangeMethod.changeString((str)->str.replace('-','_'),someStr));
     }
 }
